e5dd4d62c0c530bb935abe243a2a808fadfe31bb
johotpocket <jo.hotpocket@gmail.com>
2017-03-13 13:27:10 -0600

    implemented full CRUD functionality

--- routes/posts.js
+++ routes/posts.js
@@ -27,8 +27,46 @@ Router.route('/')
         res.json(data);
       }
     })
-  })
+  });
 
+Router.route('/:post_id')
+  .get(function(req, res){
+    Post.findById(req.params.post_id, function(err, data){
+      if(err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    })
+  })
+  .delete(function(req, res){
+    Post.remove({ _id: req.params.post_id }, function(err){
+      if(err){
+        console.log(err)
+      } else {
+        res.json({message: "successfully deleted the blog entry!"})
+      }
+    })
+  })
+  .put(function(req, res){
+    Post.findById(req.params.post_id, function(err, post){
+      if(err){
+        console.log(err)
+      } else {
+        post.content = req.body.content ? req.body.content: post.content;
+        post.title = req.body.title ? req.body.title: post.title;
+
+        post.save(function(err, updatedPost){
+          if(err){
+            console.log(err)
+          } else {
+            res.json(updatedPost);
+          }
+        });
+
+      }
+    })
+  });
 
 
 
